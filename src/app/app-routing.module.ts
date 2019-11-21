b1ee425be15aa944b0d7f5ae28b67f96bde9d234
Zachary Shorts <shorts23@gmail.com>
2019-11-20 21:09:20 -0600

    set up the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ResultsComponent} from './components/results/results.component';
+import {MainComponent} from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/' },
+  { path: '', component: MainComponent, },
+  { path: 'results', component: ResultsComponent, },
+  { path: '**', component: MainComponent, },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
