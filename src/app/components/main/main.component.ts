c1fe2e4baa8fef82a72c262b0df3e72ea1e31c3a
Zachary Shorts <shorts23@gmail.com>
2019-11-23 10:53:39 -0600

    pairs are made correctly now

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,38 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
 })
 export class MainComponent implements OnInit {
   participants = [];
   shuffledParticipants = [];
   pairedParticipants = [];
-  inputName = '';
-  inputEmail = '';
-  spouseName = '';
-  spouseEmail = '';
+  inputName = "";
+  inputEmail = "";
+  spouseName = "";
+  spouseEmail = "";
   nameForm = new FormGroup({
     name: new FormControl(this.inputName, Validators.required),
     spouseName: new FormControl(this.spouseName),
-    email: new FormControl(this.inputEmail, [Validators.required, Validators.email]),
-    spouseEmail: new FormControl(this.spouseEmail, Validators.email),
-
+    email: new FormControl(this.inputEmail, [
+      Validators.required,
+      Validators.email
+    ]),
+    spouseEmail: new FormControl(this.spouseEmail, Validators.email)
   });
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  addParticipant(name: string, email: string, spouseName?: string, spouseEmail?: string) {
-    if(spouseName !== '' && spouseEmail !== '') {
+  addParticipant(
+    name: string,
+    email: string,
+    spouseName?: string,
+    spouseEmail?: string
+  ) {
+    if (spouseName !== null && spouseEmail !== null) {
       this.participants.push({
-        name, email, spouseName,
+        name,
+        email,
+        spouseName
       });
       this.participants.push({
         name: spouseName,
@@ -41,27 +49,28 @@ export class MainComponent implements OnInit {
       });
     } else {
       this.participants.push({
-        name, email
+        name,
+        email,
+        spouseName: ""
       });
     }
+    this.nameForm.reset();
   }
 
   makePairs() {
-    for(let i = 0; i < this.participants.length; i++) {
-      if(i !== this.participants.length-1){
-          this.pairedParticipants.push({
-              giver: this.shuffledParticipants[i],
-              receiver: this.shuffledParticipants[i+1],
-          })
+    for (let i = 0; i < this.participants.length; i++) {
+      if (i !== this.participants.length - 1) {
+        this.pairedParticipants.push({
+          giver: this.shuffledParticipants[i],
+          receiver: this.shuffledParticipants[i + 1]
+        });
       } else {
-          this.pairedParticipants.push({
-              giver: this.shuffledParticipants[i],
-              receiver: this.shuffledParticipants[0],
-          })
+        this.pairedParticipants.push({
+          giver: this.shuffledParticipants[i],
+          receiver: this.shuffledParticipants[0]
+        });
       }
-
     }
-    console.log(this.pairedParticipants);
   }
 
   shuffle(participantArray: string[]) {
@@ -69,38 +78,71 @@ export class MainComponent implements OnInit {
     let temporaryValue, randomIndex;
 
     while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        temporaryValue = participantArray[currentIndex];
-        participantArray[currentIndex] = participantArray[randomIndex];
-        participantArray[randomIndex] = temporaryValue;
+      temporaryValue = participantArray[currentIndex];
+      participantArray[currentIndex] = participantArray[randomIndex];
+      participantArray[randomIndex] = temporaryValue;
     }
-    this.makePairs();
     return participantArray;
   }
 
-  submitAllNames() { 
+  submitAllNames() {
     this.shuffledParticipants = this.shuffle(this.participants);
-    for(let i = 0; i < this.shuffledParticipants.length; i++) {
-      
-    }
-    console.log(this.shuffledParticipants);
+    this.verifyPairs();
+    this.makePairs();
+    console.log(this.pairedParticipants);
   }
+
+  verifyPairs() {
+    let pairedCorrectly = false;
+    for (let i = 0; i < this.shuffledParticipants.length; i++) {
+      if (i !== this.shuffledParticipants.length - 1) {
+        if (
+          this.shuffledParticipants[i].spouseName ===
+            this.shuffledParticipants[i + 1].name ||
+          this.shuffledParticipants[i].name ===
+            this.shuffledParticipants[i + 1].spouseName
+        ) {
+          i = -1;
+          this.shuffle(this.shuffledParticipants);
+          continue;
+        }
+      } else {
+        if (
+          this.shuffledParticipants[i].spouseName ===
+            this.shuffledParticipants[0].name ||
+          this.shuffledParticipants[i].name ===
+            this.shuffledParticipants[0].spouseName
+        ) {
+          i = -1;
+          this.shuffle(this.shuffledParticipants);
+          continue;
+        }
+      }
+    }
+  }
+  
 
   sendEmail() {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      Accept: '*/*',
+      "Content-Type": "application/json",
+      Accept: "*/*"
     });
-    const options = {headers};
+    const options = { headers };
     const body = {
       name: this.inputName,
       to: this.inputEmail,
-      from: 'santaclaus@gmail.com',
-      message: 'you will be giving to '
+      from: "santaclaus@gmail.com",
+      message: "you will be giving to "
     };
-    return this.http.post('https://zacharyshorts-emailserver.herokuapp.com/sendsantaemail', body, options).subscribe();
+    return this.http
+      .post(
+        "https://zacharyshorts-emailserver.herokuapp.com/sendsantaemail",
+        body,
+        options
+      )
+      .subscribe();
   }
 }
